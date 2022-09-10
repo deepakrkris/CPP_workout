986bd1374c2ad2f58787eb8dcbeaba9b07462e23
Deepak Rajamohan <deepak.r.kris@Gmail.com>
2022-09-10 12:33:28 -0700

    learn cpp

--- dependencies/interfaces/utilities.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <xms.hpp>
-
-namespace xmsbridge {
-    xms::String& getValue(xms::String* ref);
-    char* displayMessage(xms::TextMessage * message);
-    xmsVOID displayException(xms::Exception & exception);
-}
